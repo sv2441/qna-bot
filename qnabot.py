0b66aafb4dc499d34a8fdcb4f321f169d56bbd13
Sandeep Vishwakarma <62471058+sv2441@users.noreply.github.com>
2023-09-12 20:07:01 +0530

    Update qnabot.py

--- qnabot.py
+++ qnabot.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import base64
+import csv
 import os
 import pandas as pd
 from langchain.llms import OpenAI
@@ -11,58 +12,88 @@ from dotenv import load_dotenv
 
 load_dotenv()
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+# os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 
-# Create a checkbox to select the model
-model_option = st.checkbox("Select Model: GPT 3.5 Turbo")
+# Initialize chat model
+model_option = st.checkbox("Select Model: GPT 3.5 Turbo",)
 if model_option:
     model = "gpt-3.5-turbo"
+    output_filename = "gpt3.5_results.csv"  # Set the output filename for GPT 3.5 Turbo
 else:
     model = "gpt-4"
+    output_filename = "gpt4_results.csv"  # Set the output filename for GPT 4
 
 # Initialize chat model
 chat_llm = ChatOpenAI(model_name=model, temperature=0.0)
 
-def generator(question, answer):
-    title_template = """
-                    for the "{question}", check the "{answer}" if it is correct, then explain why in short..
-                    """ 
+def dict_to_csv(data_list):
+    with open('data11.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Correct Answer', 'Explanation']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
+        # Check if the file is empty and write the header only if it's empty
+        is_file_empty = csvfile.tell() == 0
+        if is_file_empty:
+            writer.writeheader()
+
+        for data in data_list:
+            writer.writerow(data)
+
+def generator(df, output_filename):
+    answer_schema = ResponseSchema(name="Correct Answer", description="Correct answer Option.")
+    explanation_schema = ResponseSchema(name="Explanation", description="Explanation in one Sentence.")
+
+    response_schemas = [answer_schema, explanation_schema]
+    output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+    format_instructions = output_parser.get_format_instructions()
+    title_template = """
+                    for the "{question}", Provide the correct answer option (A, B, C, D).
+                    If none of the options apply, please select "N/A".
+                    Provide Explanation in one Sentence.
+                    {format_instructions}
+                    """ 
     prompt = ChatPromptTemplate.from_template(template=title_template)
-    messages = prompt.format_messages(question=question, answer=answer)
-    response = chat_llm(messages)
-    return str(response.content)
+    
+    data_list = []  # Create a list to store dictionaries
+
+    for index, row in df.iterrows():
+        messages = prompt.format_messages(question=row['Question'], format_instructions=format_instructions)
+        response = chat_llm(messages)
+        response_as_dict = output_parser.parse(response.content)
+        data = response_as_dict
+        data_list.append(data)  # Append the data to the list
+    
+    dict_to_csv(data_list)  # Pass the list of dictionaries to dict_to_csv
+    
+    data11 = pd.read_csv("data11.csv", encoding='cp1252')
+    results = pd.concat([df, data11], axis=1)
+    results.to_csv(output_filename, mode='a', header=not os.path.isfile(output_filename), index=False)
+    st.subheader("Results")
+    st.dataframe(results)
+    st.markdown(get_download_link(results, output_filename), unsafe_allow_html=True)
+
+def get_download_link(df, output_filename):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{output_filename}">Download CSV File</a>'
+    return href
 
 def main():
-    st.title("👨‍💻 QnA with explanation")
+    st.title("👨‍💻 Question and Answer")
+    # File upload
+    file = st.file_uploader("Upload OP's", type=["csv"])
 
-    # File Upload
-    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-    
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
+    if file is not None:
+        # Read CSV file
+        df = pd.read_csv(file)
+
+        # Display preview
+        st.subheader("CSV File Preview")
         st.dataframe(df)
-        # Add an "Explanation" column
-        df["Explanation"] = ""
 
-        # Process each row
-        for index, row in df.iterrows():
-            question = row["Question"]
-            answer = row["Answer"]
-            explanation = generator(question, answer)
-            df.at[index, "Explanation"] = explanation
-
-        # Display the result DataFrame
-        st.write("Result:")
-        result_filename = f"{model.lower().replace(' ', '')}_result"
-        df.to_csv(result_filename)
-        st.write(df)
-
-        # Create a download link for the result CSV
-        csv_file = df.to_csv(index=False)
-        b64 = base64.b64encode(csv_file.encode()).decode()
-        st.markdown(f'### Download Result CSV ###')
-        href = f'<a href="data:file/csv;base64,{b64}" download="{result_filename}">Click here to download the result CSV file</a>'
-        st.markdown(href, unsafe_allow_html=True)
-
+        # Button to process the file
+        if st.button("Generate answers"):
+            generator(df, output_filename)
+            
 if __name__ == "__main__":
     main()
